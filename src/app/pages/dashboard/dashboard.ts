910056407bf05e47c7b8a92dbb886fe862fc0a7c
Daniel37UwU <danielnog37@gmail.com>
2026-07-14 18:53:18 -0600

    Completado Sprint 1: UI de Login y Dashboard con Angular Material

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
-  templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
+  standalone: true,
+  imports: [
+    MatToolbarModule, 
+    MatCardModule, 
+    MatButtonModule, 
+    MatIconModule
+  ], // Importamos los componentes visuales necesarios
+  templateUrl: './dashboard.html', // o './dashboard.component.html'
+  styleUrl: './dashboard.css'      // o './dashboard.component.css'
 })
-export class Dashboard {}
+export class Dashboard {
+
+  constructor(private router: Router) {}
+
+  cerrarSesion() {
+    // Te regresa de forma segura a la pantalla de Login
+    this.router.navigate(['/login']);
+  }
+}
